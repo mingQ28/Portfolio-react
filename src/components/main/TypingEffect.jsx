9d0c9422590dcbdb60ec3a6bc88af8e4614563b5
mingQ28 <eo77si9@naver.com>
2025-08-27 17:52:11 +0900

    style:

--- src/components/main/TypingEffect.jsx
+++ src/components/main/TypingEffect.jsx
@@ -3,21 +3,17 @@ import { ReactTyped } from "react-typed";
 
 const TypingEffect = () => {
   return (
-    <div className="mt-80 mr-30 text-right">
+    <div className="flex mt-50 flex-col items-center space-y-2 px-5 py-10 text-center">
       <ReactTyped
         strings={["안녕하세요.&nbsp; <br>프론트엔드 개발자 박민서입니다."]}
         typeSpeed={70} // 타이핑 속도
         backSpeed={25} // 타이핑 지우는 속도
         loop={false} // 반복 안함
         style={{
-          fontSize: "70px",
-          fontWeight: 600,
-          lineHeight: "1.2em", // span 기준으로 높이 적용
-          verticalAlign: "middle",
+          fontSize: "50px",
         }}
       />
     </div>
   );
 };
-
 export default TypingEffect;
