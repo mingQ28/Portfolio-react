80b6e6bef7c091153e1931bc9afb07c3e6dda10d
mingP28 <eo77si9@naver.com>
2025-08-31 21:06:55 +0900

    style: 간격 조절

--- src/components/main/TypingEffect.jsx
+++ src/components/main/TypingEffect.jsx
@@ -3,9 +3,9 @@ import { ReactTyped } from "react-typed";
 
 const TypingEffect = () => {
   return (
-    <div className="flex mt-50 flex-col items-center space-y-2 px-5 py-10 text-center">
+    <div className="flex mt-70 flex-col items-center space-y-2 px-5 py-10 text-center">
       <ReactTyped
-        strings={["안녕하세요.&nbsp; <br>프론트엔드 개발자 박민서입니다."]}
+        strings={["안녕하세요. <br>프론트엔드 개발자 박민서입니다."]}
         typeSpeed={70} // 타이핑 속도
         backSpeed={25} // 타이핑 지우는 속도
         loop={false} // 반복 안함
